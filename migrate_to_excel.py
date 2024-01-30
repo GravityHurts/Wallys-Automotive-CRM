7ac3704f4cff3458da41122d44afc430f1076125
RJ <furryhead14@gmail.com>
2024-01-30 10:01:07 -0500

    Migrate to excel script + pip install

---
+++ migrate_to_excel.py
@@ -0,0 +1,20 @@
+import sqlite3
+import pandas as pd
+
+def sqlite_to_excel(database_file, excel_file):
+    conn = sqlite3.connect(database_file)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+
+    with pd.ExcelWriter(excel_file) as writer:
+        for table in tables:
+            table_name = table[0]
+            df = pd.read_sql_query(f"SELECT * FROM {table_name};", conn)
+            df.to_excel(writer, sheet_name=table_name, index=False)
+
+    cursor.close()
+    conn.close()
+
+sqlite_to_excel('mechanic_app.db', 'output.xlsx')
